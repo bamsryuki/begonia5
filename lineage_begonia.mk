f07d1b5a0b0b8a263c1e8a4e9148bb452e0af66e
AndroidTanz <archeartve@gmail.com>
2022-06-09 04:23:00 +0000

    begonia: Bringup riceDroid

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -16,8 +16,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Inherit from begonia device
 $(call inherit-product, device/redmi/begonia/device.mk)
 
-# Inherit some common PixelPlusUI stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common riceDroid stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
@@ -36,24 +36,15 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_BOOTANIMATION_PRELOAD := true
-TARGET_BOOTANIMATION_TEXTURE_CACHE := true
-TARGET_BOOTANIMATION_MULTITHREAD_DECODE := true
-TARGET_BOOTANIMATION_HALF_RES := true
 
-# PixelPlusUI Flags
-CUSTOM_BUILD_TYPE := OFFICIAL
-PPUI_MAINTAINER := 7Soldier
-IS_PHONE := true
+# riceDroid Flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ARCORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP  := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := begonia
-PRODUCT_NAME := aosp_begonia
+PRODUCT_NAME := lineage_begonia
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 8 Pro
 PRODUCT_MANUFACTURER := Xiaomi
