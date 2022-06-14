1b56af9de820871b99119789655d2863895f4529
AndroidTanz <archeartve@gmail.com>
2022-06-14 03:19:29 +0000

    begonia: Debloated made ready for Vanilla

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -21,8 +21,6 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
-$(call inherit-product, vendor/gcambsg/config.mk)
-$(call inherit-product, vendor/v4afx/config.mk)
 $(call inherit-product, vendor/dirac/config.mk)
 $(call inherit-product, vendor/ANXCamera/config.mk)
 
--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -1,15 +1,22 @@
 LOCAL_PATH := $(call my-dir)
-
+  
 include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := PartnerSetupPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SecurityHubPrebuilt
-LOCAL_OVERRIDES_PACKAGES += GoogleFeedback HelpRtcPrebuilt
-LOCAL_OVERRIDES_PACKAGES += Camera2
-LOCAL_OVERRIDES_PACKAGES += SoundAmplifierPrebuilt PixelBuds
-LOCAL_OVERRIDES_PACKAGES += GoogleTTS
+LOCAL_OVERRIDES_PACKAGES += AboutBliss AmbientSensePrebuilt AppDirectedSMSService arcore
+LOCAL_OVERRIDES_PACKAGES += Calendar CalendarGooglePrebuilt Camera2 CarrierSetup Chrome Chrome-Stub ConnMO ConnMetrics
+LOCAL_OVERRIDES_PACKAGES += DCMO DevicePolicyPrebuilt DMService
+LOCAL_OVERRIDES_PACKAGES += Gallery2 GCS GoogleCamera Jelly
+LOCAL_OVERRIDES_PACKAGES += MaestroPrebuilt Maps MyVerizonServices NovaBugReportWrapper Ornament OemDmTrigger OBDM_Perm obdm_stub
+LOCAL_OVERRIDES_PACKAGES += Phonograph Photos PixelLiveWallpaperPrebuilt
+LOCAL_OVERRIDES_PACKAGES += RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SCONE ScribePrebuilt Showcase SimpleCalculator SimpleCalendar Snap SoundAmplifierPrebuilt SprintDM SprintHM
+LOCAL_OVERRIDES_PACKAGES += Tag Tycho
+LOCAL_OVERRIDES_PACKAGES += USCCDM
+LOCAL_OVERRIDES_PACKAGES += Videos VZWAPNLib VzwOmaTrigger
+LOCAL_OVERRIDES_PACKAGES += WallpapersBReel2020
+LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
