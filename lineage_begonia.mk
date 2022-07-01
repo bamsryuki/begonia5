e391d9aacf36a8b67cf256c853502546c3a7adc3
blazerpaul15 <blazerpaul15@gmail.com>
2022-07-01 14:24:49 +0530

    begonia: Bringup riceDroid

--- lineage_begonia.mk
+++ lineage_begonia.mk
@@ -13,11 +13,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Inherit from begonia device
-$(call inherit-product, device/redmi/begonia/device.mk)
-
 # Inherit some common riceDroid stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Rom flags
+RICE_MAINTAINER := Paul
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := false
+TARGET_ENABLE_BLUR := true
+WITH_GMS := true
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
@@ -27,22 +36,8 @@ $(call inherit-product, vendor/dirac/config.mk)
 RELAX_USES_LIBRARY_CHECK := true
 PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 
-# Screen density
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# RiceDroid Flags
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_SUPPORTS_QUICK_TAP  := true
-TARGET_ENABLE_BLUR := true
-TARGET_BUILD_GRAPHENEOS_CAMERA := true
-WITH_GAPPS := true
-RICE_MAINTAINER := Tanz Archeart
-RICE_OFFICIAL := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := begonia
